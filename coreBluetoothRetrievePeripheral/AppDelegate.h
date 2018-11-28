514b21bf470798c6cc2071ede5d848fcb7ff95ac
ZhangYi <zhangyi@codoon.com>
2018-11-28 12:11:11 +0800

    Initial Commit

---
+++ coreBluetoothRetrievePeripheral/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  coreBluetoothRetrievePeripheral
+//
+//  Created by ZhangYi on 2018/11/28.
+//  Copyright © 2018 ZhangYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ coreBluetoothRetrievePeripheral/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  coreBluetoothRetrievePeripheral
+//
+//  Created by ZhangYi on 2018/11/28.
+//  Copyright © 2018 ZhangYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
